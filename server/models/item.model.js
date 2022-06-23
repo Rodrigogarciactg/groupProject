413a7b73170dcc11efb071cfdc46165e7303e701
Omar Ochoa <omarochoa19@yahoo.com>
2022-06-23 13:44:10 -0700

    added functioanlity

---
+++ server/models/item.model.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const ItemsSchema = {
+    item: {
+        type: String,
+        required: [true, "name is required"],
+        minLength: [3, "Name must be at least 3 characters"],
+    },
+    price: {
+        type: String,
+        required: [true, "type is required"],
+        minLength: [3, "Type must be at least 3 characters"],
+    },
+    seller: {
+        type: String,
+        required: [true, "description is required"],
+        minLength: [3, "Type must be at least 3 characters"],
+    },
+    // skill1: {
+    //     type: String,
+    //     required: [true, "type is required"],
+    //     minLength: [3, "Type must be at least 3 characters"],
+    // },
+    // skill2: {
+    //     type: String,
+    //     required: [true, "type is required"],
+    //     minLength: [3, "Type must be at least 3 characters"],
+    // },
+    // skill3: {
+    //     type: String,
+    //     required: [true, "type is required"],
+    //     minLength: [3, "Type must be at least 3 characters"],
+    // },
+};
+
+module.exports = mongoose.model("Item", ItemsSchema);
